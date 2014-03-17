8c8f973a088db943aea155f1afd694ef0a932140
Brian Luft <brian@electroly.com>
2014-03-17 01:45:12 -0500

    Subclassed FLTK widgets.

---
+++ ospasoft/include/FxToolkit.h
@@ -0,0 +1,63 @@
+// OSPASOFT
+// Copyright (C) 2014 Brian Luft
+// 
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
+// License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+// details.
+// 
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#pragma once
+
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Return_Button.H>
+#include <FL/Fl_Menu_Item.H>
+#include "SpEvent.h"
+
+class FxEventArgs : public SpEventArgs
+{
+public:
+   FxEventArgs();
+   bool Handled;
+};
+
+class FxMenuItem
+{
+public:
+   void Attach(Fl_Menu_Item* flMenuItem);
+
+   SpEvent<SpEventArgs> Clicked;
+
+private:
+   static void StaticCallback(Fl_Widget*, void* ptr);
+};
+
+class FxWindow : public Fl_Double_Window
+{
+public:
+   FxWindow(int W, int H, const char* l = 0);
+   FxWindow(int X, int Y, int W, int H, const char* l = 0);
+   virtual int handle(int event);
+
+   SpEvent<FxEventArgs> Closed;
+   SpEvent<FxEventArgs> Activated;
+   SpEvent<FxEventArgs> Deactivated;
+};
+
+class FxReturnButton : public Fl_Return_Button
+{
+public:
+   FxReturnButton(int X, int Y, int W, int H, const char* l = 0);
+   virtual int handle(int event);
+
+   SpEvent<SpEventArgs> Clicked;
+
+private:
+   static void StaticCallback(Fl_Widget*, void* ptr);
+};
---
+++ ospasoft/src/FxToolkit.cpp
@@ -0,0 +1,88 @@
+// OSPASOFT
+// Copyright (C) 2014 Brian Luft
+// 
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public 
+// License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+// details.
+// 
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#include "FxToolkit.h"
+
+#define FX_BEGIN_HANDLE(className, superclassName) \
+   int className ::handle(int event) \
+   { \
+      if (event < 0) \
+         return superclassName ::handle(-event); \
+      switch (event) \
+      { \
+         default: \
+            return superclassName ::handle(event);
+
+#define FX_HANDLE_EVENT(fltkEventType, spEventObj) \
+         case fltkEventType : \
+         { \
+            FxEventArgs e; \
+            spEventObj .Fire(e); \
+            if (e.Handled) \
+               return 1; \
+            else \
+               return handle(-event); \
+         }
+
+#define FX_END_HANDLE() \
+      } \
+   }
+
+#define FX_CALLBACK(className, spEventObj) \
+   void className ::StaticCallback(Fl_Widget*, void* ptr) \
+   { \
+      className * self = static_cast< className *>(ptr); \
+      SpEventArgs e; \
+      self-> spEventObj .Fire(e); \
+   }
+
+FxEventArgs::FxEventArgs() : Handled(false) {}
+
+//
+// FxMenuItem
+//
+void FxMenuItem::Attach(Fl_Menu_Item* flMenuItem)
+{
+   flMenuItem->callback(StaticCallback, this);
+}
+
+void FxMenuItem::StaticCallback(Fl_Widget*, void* ptr)
+{
+   SpEventArgs e;
+   ((FxMenuItem*)ptr)->Clicked.Fire(e);
+}
+
+//
+// FxWindow
+//
+FxWindow::FxWindow(int W, int H, const char* l) : Fl_Double_Window(W, H, l) {}
+FxWindow::FxWindow(int X, int Y, int W, int H, const char* l) : Fl_Double_Window(X, Y, W, H, l) {}
+FX_BEGIN_HANDLE(FxWindow, Fl_Double_Window)
+FX_HANDLE_EVENT(FL_CLOSE, Closed)
+FX_HANDLE_EVENT(FL_ACTIVATE, Activated)
+FX_HANDLE_EVENT(FL_DEACTIVATE, Deactivated)
+FX_END_HANDLE()
+
+//
+// FxReturnButton
+//
+FxReturnButton::FxReturnButton(int X, int Y, int W, int H, const char* l) 
+: Fl_Return_Button(X, Y, W, H, l) 
+{
+   callback(StaticCallback, this);
+}
+FX_BEGIN_HANDLE(FxReturnButton, Fl_Return_Button)
+FX_END_HANDLE()
+FX_CALLBACK(FxReturnButton, Clicked)
+
