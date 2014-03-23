d55d9df371b1af20f610b97784328027c1b44bd8
Brian Luft <brian@electroly.com>
2014-03-23 07:37:55 -0500

    Perhaps this should be C# instead.

---
+++ ProgDev/IDE/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ProgDev.IDE
+{
+   partial class MainForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+         this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+         this.menuStrip1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // menuStrip1
+         // 
+         this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+         this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+         this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+         this.menuStrip1.Name = "menuStrip1";
+         this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+         this.menuStrip1.TabIndex = 1;
+         this.menuStrip1.Text = "menuStrip1";
+         // 
+         // fileToolStripMenuItem
+         // 
+         this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+         this.fileToolStripMenuItem.Text = "&File";
+         // 
+         // statusStrip1
+         // 
+         this.statusStrip1.BackColor = System.Drawing.SystemColors.Control;
+         this.statusStrip1.Location = new System.Drawing.Point(0, 413);
+         this.statusStrip1.Name = "statusStrip1";
+         this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+         this.statusStrip1.Size = new System.Drawing.Size(484, 22);
+         this.statusStrip1.TabIndex = 2;
+         this.statusStrip1.Text = "statusStrip1";
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(484, 435);
+         this.Controls.Add(this.statusStrip1);
+         this.Controls.Add(this.menuStrip1);
+         this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.IsMdiContainer = true;
+         this.MainMenuStrip = this.menuStrip1;
+         this.Name = "MainForm";
+         this.Text = "OSPA ProgDev";
+         this.menuStrip1.ResumeLayout(false);
+         this.menuStrip1.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.MenuStrip menuStrip1;
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+      private System.Windows.Forms.StatusStrip statusStrip1;
+   }
+}
+
