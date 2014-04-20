68e67ef75c88dcb36ca12668d5d0282809b7c408
Brian Luft <brian@electroly.com>
2014-04-20 03:46:31 -0500

    Bug fix in the installer build script.

--- src/BuildInstaller.bat
+++ src/BuildInstaller.bat
@@ -1,8 +1,11 @@
+@echo off
+prompt $
+@echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-msbuild /target:clean /verbosity:q /nologo
-msbuild /target:build /verbosity:q /nologo
+msbuild /target:clean /p:Configuration=Release /verbosity:q /nologo
+msbuild /target:build /p:Configuration=Release /verbosity:q /nologo
 pushd ProgDev
-msbuild /target:publish /verbosity:q /nologo
+msbuild /target:publish /p:Configuration=Release /verbosity:q /nologo
 popd
 "C:\Program Files (x86)\NSIS\makensis.exe" /V3 Installer.nsi
 pause
